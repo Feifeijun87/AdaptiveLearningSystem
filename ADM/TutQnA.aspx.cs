1bc7453bad083dc17c6f89bac8f4eca35ea627c4
arieljy <arieljy@LAPTOP-G1MU5A5H>
2018-09-23 01:41:33 +0800

    webconfig added connection string and aspx code in TutQnA

---
+++ ADM/TutQnA.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+
+namespace ADM
+{
+    public partial class TutQnA : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ConnectDB();
+        }
+
+        public void ConnectDB()
+        {
+            SqlConnection con = new SqlConnection(WebConfigurationManager.ConnectionStrings["fyp"].ConnectionString);
+            con.Open();
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                DisplayMessage(this, "Connection to the database successful");
+            }
+        }
+
+        static public void DisplayMessage(Control page, string msg)
+        {
+            string myScript = String.Format("alert('{0})", msg);
+            ScriptManager.RegisterStartupScript(page, page.GetType(), "MyScript", myScript, true);
+        }
+    }
+}
